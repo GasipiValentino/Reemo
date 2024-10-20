76868a7d3d51c9d4a22c996d376bbf151014fae3
lucasn.panadero <lucasn.panadero@gmail.com>
2024-10-20 14:36:52 +0000

    Add Storage & Reservations

--- src/pages/ProfileEdit.vue
+++ src/pages/ProfileEdit.vue
@@ -55,7 +55,7 @@ export default{
           this.$router.push('/Profile');
         }
       },
-      // Previsualizar y guardar el archivo seleccionado
+      // Previsualizar y guardar el archivo seleccionado hola
       handleFileSelection(ev) {
         this.editData.photo = ev.target.files[0];
         const reader = new FileReader();
